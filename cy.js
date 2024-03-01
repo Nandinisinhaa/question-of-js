7108f835269b6db2fe9802fc6ab2227f903bf7f4
Nandinisinhaa <161804019+Nandinisinhaa@users.noreply.github.com>
2024-03-01 14:15:43 +0530

    Add files via upload

---
+++ cy.js
@@ -0,0 +1,8 @@
+const input = require("readline-sync")
+let a = input.questionInt("enter date: ")
+let b = input.questionInt("enter month: ")
+let c = input.questionInt("enter year: ")
+
+console.log(a)
+console.log(b)
+console.log(c)
